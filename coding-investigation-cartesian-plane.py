a8ec03ee039af3f2bc43082c72c93843d8cca4d1
DrDuck_27 <83153830+thumindu007@users.noreply.github.com>
2023-05-11 13:33:29 +0000

    made a function that moves players also cleaned up

--- coding-investigation-cartesian-plane.py
+++ coding-investigation-cartesian-plane.py
@@ -7,48 +7,66 @@ def calculate_midpoint(player1,player2):
     midpointx,midpointy = (player1['posx']+player2['posx'])/2, (player1['posy']+player2['posy'])/2
     return midpointx, midpointy
 
-def calculate_gradient(playercords,destination):
-    return round((destination['posy']-playercords['posy']) / (destination['posx']-playercords['posx']), 1)
+def calculate_gradient(playercoords,destination):
+    return round((destination['posy']-playercoords['posy']) / (destination['posx']-playercoords['posx']), 1)
 
 def calculate_distance(point1, point2):
     return round(math.sqrt(((point1['posx'] - point2['posx']) ** 2) + ((point1['posy'] - point2['posy']) ** 2)), 1)
 
 def print_info(player, otherplayer, destination):
-    print("     **PLAYER",player['playertype'],"INFO**     "
-          '\nLocation:',player['posx'],"x",player['posy'],
-          "y\nDistance to Destination:",calculate_distance(player, destination),
-          'units\nGradient with destination:',calculate_gradient(player, destination),
-          '\nMidpoint with Player',otherplayer['playertype'],
-          ':',calculate_gradient(player1, player2),'\n\n')
+    print("\n     **PLAYER",player['playertype'],"INFO**     ")
+    print('Location:',player['posx'],"x",player['posy'],'y')
+    print("Distance to Destination:",calculate_distance(player, destination),'units')
+    print('Gradient with destination:',calculate_gradient(player, destination))
+    print('Midpoint with Player',otherplayer['playertype'],':',calculate_midpoint(player1, player2),'\n')
 
 def print_destination_info(destination):
-    print("     **DESTINATION LOCATION**     ",
-          '\nLocation:',destination['posx'],"x",destination['posy'],"y")
+    print("\n     **DESTINATION LOCATION**     ",
+          '\nLocation:',destination['posx'],"x",destination['posy'],"y\n\n")
 
-def move_player(player):
+def translation_calculator(short, long, direction):
+    if direction == 1: return long, short
+    elif direction == 2: return short, long
+    elif direction == 3: return -short, long
+    elif direction == 4: return -long, short
+    elif direction == 5: return -long, -short
+    elif direction == 6: return -short, -long
+    elif direction == 7: return short, -long
+    elif direction == 8: return long, -short
+
+def move_player(triples):
     units_to_move = int(input("How much units do you want to move: "))
     direction = int(input("What direction will you like to move"))
-    for i in range(units_to_move,0,-1):
-        print(i)
-        for x in range(len):
-            
-        if i == triples[0][2]:
+    for i in range(len(triples)):
+        if triples[i][2] > units_to_move:
             break
+        triple_to_move = triples[i]
+    short,long = triple_to_move[0], triple_to_move[1]
+    return translation_calculator(short, long, direction)
     print("end")
     
 #assigns random cords to each player and destination
-player1 = {'posx':random.randrange(-800, 800, 1), 
-            'posy':random.randrange(-800, 800, 1), 
+player1 = {'posx' : random.randrange(-800, 800, 1), 
+            'posy' : random.randrange(-800, 800, 1), 
             'playertype' : 1}
-player2 = {'posx':random.randrange(-800, 800, 1), 
-            'posy':random.randrange(-800, 800, 1), 
+player2 = {'posx' : random.randrange(-800, 800, 1), 
+            'posy' : random.randrange(-800, 800, 1), 
             'playertype' : 2}
-destination = {'posx':random.randrange(-800, 800, 1), 
-               'posy':random.randrange(-800, 800, 1),
+destination = {'posx' : random.randrange(-800, 800, 1), 
+               'posy' : random.randrange(-800, 800, 1),
                 'playertype' : 3}
+
 #prints all the player info
 print_info(player1, player2, destination)
 print_info(player2, player1, destination)
 print_destination_info(destination)
-move_player(player1)
-print()
+
+while True:
+    print("Player 1, enter your direction and the amount you want to move in it")
+    new_coords = move_player(triples)
+    player1.update({'posx' : player1['posx'] + new_coords[0], 'posy' : player1['posy'] + new_coords[1]})
+    print_info(player1, player2, destination)
+    print("Player 2, enter your direction and the amount you want to move in it")
+    new_coords = move_player(triples)
+    player2.update({'posx' : player2['posx'] + new_coords[0], 'posy' : player2['posy'] + new_coords[1]})
+    print_info(player2, player1, destination)

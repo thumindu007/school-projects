e9f6f86b1f390b3529b7e4e41dd8f4030b87b474
DrDuck_27 <83153830+thumindu007@users.noreply.github.com>
2023-05-14 13:02:35 +0000

    implemented buffer fully

--- coding-investigation-cartesian-plane.py
+++ coding-investigation-cartesian-plane.py
@@ -2,6 +2,28 @@ import random
 import math
 from triples import *
 
+class Player:
+    def __init__(self, posx, posy, playertype):
+        self.posx = posx
+        self.posy = posy
+        self.playertype = playertype
+        self.spacebuffer = 10
+
+    def print_info(self, destination, otherplayer):
+        print("\n     **PLAYER",self.playertype,"INFO**     ")
+        print('Location:',self.posx,"x",self.posy,'y')
+        print("Distance to Destination:",calculate_distance(self, destination),'units')
+        print('Gradient with destination:',calculate_gradient(self, destination))
+        print('Midpoint with other Player:',calculate_midpoint(self, otherplayer),'\n')
+
+    def check_win(self, otherplayer, destination):
+        dis_otherplayer = calculate_distance(self, otherplayer)
+        dis_destination = calculate_distance(self, destination)
+        if dis_otherplayer < self.spacebuffer or dis_destination < self.spacebuffer:
+            return True
+        else:
+            return False
+
 # calculates the midpoint between two points given to the function
 def calculate_midpoint(player1,player2):
     midpointx,midpointy = (player1.posx + player2.posx) / 2, (player1.posy + player2.posy)/2
@@ -27,37 +49,21 @@ def translation_calculator(short_side, long_side, direction):
     print(short_side, long_side)
     return short_side, long_side
 
-def move_player(triples):
-    units_to_move = int(input("How much units do you want to move: "))
-    direction = int(input("What direction will you like to move"))
-    for i in range(len(triples)):
-        if triples[i][2] > units_to_move:
+def move_player(possible_moves):
+    units_to_move_direction = input("How much units do you want to move: ")
+    units_to_move_direction = units_to_move_direction.split()
+    units_to_move, direction = int(units_to_move_direction[0]), int(units_to_move_direction[1])
+ 
+    for move in possible_moves:
+        if move[2] > units_to_move:
             break
-        triple_to_move = triples[i]
+        triple_to_move = move
     short,long = triple_to_move[0], triple_to_move[1]
+
     return translation_calculator(short, long, direction)
 
 #creates a class for the template of the players
-class player:
-    def __init__(self, posx, posy, playertype):
-        self.posx = posx
-        self.posy = posy
-        self.playertype = playertype
 
-    def print_info(self, destination, otherplayer):
-        print("\n     **PLAYER",self.playertype,"INFO**     ")
-        print('Location:',self.posx,"x",self.posy,'y')
-        print("Distance to Destination:",calculate_distance(self, destination),'units')
-        print('Gradient with destination:',calculate_gradient(self, destination))
-        print('Midpoint with other Player:',calculate_midpoint(self, otherplayer),'\n')
-
-    def checkspacebuffer(self, otherplayer, destination):
-        dis_otherplayer = calculate_distance(self, otherplayer)
-        dis_destination = calculate_distance(self, destination)
-        if dis_destination < 10 or dis_destination < 10:
-            return True
-        else:
-            return False
 
 directions = [[1, 1, 1, True],
               [2, 1, 1, False],
@@ -69,17 +75,17 @@ directions = [[1, 1, 1, True],
               [8, 1, -1, True]]
 
 #uses the class as a template to create players with random coordinates
-player1 = player(random.randrange(-800, 800, 1), random.randrange(-800, 800, 1), 1)
-player2 = player(random.randrange(-800, 800, 1), random.randrange(-800, 800, 1), 2)
-destination = player(random.randrange(-800, 800, 1), random.randrange(-800, 800, 1), 3)
+player1 = Player(random.randrange(-800, 800, 1), random.randrange(-800, 800, 1), 1)
+player2 = Player(random.randrange(-800, 800, 1), random.randrange(-800, 800, 1), 2)
+target_destination = Player(random.randrange(-800, 800, 1), random.randrange(-800, 800, 1), 3)
 
 player1win = False
 player2win = False
 
 #prints all the player info
-player1.print_info(destination, player2)
-player2.print_info(destination, player1)
-print_destination_info(destination)
+player1.print_info(target_destination, player2)
+player2.print_info(target_destination, player1)
+print_destination_info(target_destination)
 
 
 
@@ -88,15 +94,18 @@ while player1win == False and player2win == False:
     print("Player 1, enter your direction and the amount you want to move in it")
     new_coords = move_player(triples)
     player1.posx, player1.posy = player1.posx + new_coords[0], player1.posy + new_coords[1]
-    player1.print_info(destination, player2)
-    if player1.checkspacebuffer(player2, destination):
+    player1.print_info(target_destination, player2)
+    if player1.check_win(player2, target_destination):
+        winner = 'Player 1'
         player1win = True
-
+    print_destination_info(target_destination)
     print("Player 2, enter your direction and the amount you want to move in it")
     new_coords = move_player(triples)
     player2.posx, player2.posy = player2.posx + new_coords[0], player2.posy + new_coords[1]
-    player2.print_info(destination, player2)
-    if player2.checkspacebuffer(player1, destination):
+    player2.print_info(target_destination, player2)
+    print_destination_info(target_destination)
+    if player2.check_win(player1, target_destination):
+        winner = 'Player 2'
         player1win = True
 
-print("win")
+print(winner,"has won the game!")
